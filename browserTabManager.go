0e46a29b53071fe3d9ff5c7a9695da64a12bff30
Vivek-Patel3 <vivekvibha310@gmail.com>
2026-02-18 14:42:40 +0530

    Implement search functionality wherein the program will return the list of tabs matching the given prefix - only insert tabs with titles using a-z (have kept it simple for now) - future work: use map instead of slice for storing the character

--- browserTabManager.go
+++ browserTabManager.go
@@ -11,22 +11,26 @@ type Tab struct {
 }
 
 type TabManager struct {
-	list *linkedlist.LinkedList[Tab]
-	trie trie.Trie
+	list *linkedlist.LinkedList[*Tab] // Tab: shared resource, so store it by reference
+	trie *trie.Trie
 }
 
-func NewTab() *Tab {
-	return &Tab{}
+func NewTab(title, url string) *Tab {
+	return &Tab{
+		title: title,
+		url: url,
+	}
 }
 
 func NewTabManager() *TabManager {
 	return &TabManager{
-		list: linkedlist.NewLinkedList[Tab](),
+		list: linkedlist.NewLinkedList[*Tab](),
+		trie: trie.NewTrie(),
 	}
 }
 
 // handle the insertion part in linkedlist
-func (tabManager *TabManager) Insert(tab Tab) {
+func (tabManager *TabManager) Insert(tab *Tab) {
 	// first insert it in linkedlist
 	nodeL := linkedlist.NewNode(tab)
 	tabManager.list.Insert(nodeL)
@@ -35,8 +39,31 @@ func (tabManager *TabManager) Insert(tab Tab) {
 	tabManager.trie.Insert(tab.title)
 }
 
-func (tabManager *TabManager) Search(name string) {
-	// need a list of tabs matching this name 
-	// therefore creating a wrapper around trie node to also store a list of tabs with the given title
+func dfs(node *trie.Node, soFar string, searchResult *[]string) {
+	f := false
+	for i, neigh := range node.Children {
+		if neigh != nil {
+			f = true
+			dfs(neigh, soFar + string(rune('a' + i)), searchResult)
+		}
+	}
+
+	if !f {
+		*searchResult = append(*searchResult, soFar)
+	}
+}
+
+func (tabManager *TabManager) Search(name string) []string {
+	// 1. get to the node which matches the prefix the most
+	// 2. do dfs/bfs to get the full name of tabs in the trie and return that list
+
+	//1. node
+	node := tabManager.trie.ReturnPrefixNode(name)
 	
+	//2. doing dfs - no need of keeping visited array (no cycles, indegree of all nodes =1)
+	searchResult := make([]string, 0)
+
+	dfs(node, name, &searchResult)
+
+	return searchResult
 }
