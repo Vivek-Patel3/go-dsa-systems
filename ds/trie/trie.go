5329b33fff40d5473678f2e852e1a5c3a957297d
Vivek-Patel3 <vivekvibha310@gmail.com>
2026-02-18 14:45:09 +0530

    Implement the method of returning node representing the end of matching prefix - Node and its fields have been exported as a result of this method

--- ds/trie/trie.go
+++ ds/trie/trie.go
@@ -1,18 +1,18 @@
 package trie
 
-type node struct {
-	children []*node
-	isLeaf bool
+type Node struct {
+	Children []*Node
+	IsLeaf bool
 }
 
 type Trie struct {
-	root *node
+	root *Node
 }
 
-func NewTrieNode() *node {
-	return &node{
-		children: make([]*node, 26),
-		isLeaf: false,
+func NewTrieNode() *Node {
+	return &Node{
+		Children: make([]*Node, 26),
+		IsLeaf: false,
 	}
 }
 
@@ -26,36 +26,49 @@ func (trie *Trie) Insert(s string) {
 	temp := trie.root
 
 	for _,c := range s {
-		if temp.children[c - 'a'] == nil {
-			// create new node
-			temp.children[c-'a'] = NewTrieNode() 
+		if temp.Children[c - 'a'] == nil {
+			// create new Node
+			temp.Children[c-'a'] = NewTrieNode() 
 		} 
-		temp = temp.children[c-'a']
+		temp = temp.Children[c-'a']
 	}
 
-	temp.isLeaf = true
+	temp.IsLeaf = true
 }
 
 func (trie *Trie) Search(s string) bool {
 	temp := trie.root
 
 	for _,c := range s {
-		if temp.children[c-'a'] == nil {
+		if temp.Children[c-'a'] == nil {
 			return false
 		}
-		temp = temp.children[c-'a']
+		temp = temp.Children[c-'a']
 	}
-	return temp.isLeaf
+	return temp.IsLeaf
 }
 
 func (trie *Trie) IsPrefix(s string) bool {
 	temp := trie.root
 
 	for _,c := range s {
-		if temp.children[c-'a'] == nil {
+		if temp.Children[c-'a'] == nil {
 			return false
 		}
-		temp = temp.children[c-'a']
+		temp = temp.Children[c-'a']
 	}
 	return true
+}
+
+func (trie *Trie) ReturnPrefixNode(s string) *Node {
+	temp := trie.root
+
+	for _,c := range s {
+		if temp.Children[c-'a'] == nil {
+			break
+		}
+		temp = temp.Children[c-'a']
+	}
+
+	return temp
 }
